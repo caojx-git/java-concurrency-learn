209a5a504dcb2d48a9a3b46e74a8a94887233f88
jianx.cao001 <jianx.cao001@bkjk.com>
2019-09-05 15:41:34 +0800

    添加ThreadLocal解决SimpleDateFormat示例

--- src/main/java/com/caojx/javaconcurrencylearn/example/commonUnsafe/SimpleDateFormatExample3.java
+++ src/main/java/com/caojx/javaconcurrencylearn/example/commonUnsafe/SimpleDateFormatExample3.java
@@ -11,22 +11,28 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 /**
- * SimpleDateFormat 测试，将SimpleDateFormat声明为局部变量达到堆栈封闭，达到多线程环境下线程安全
- * 或者使用线程封闭技术，TreadLocal也可以解决
+ * SimpleDateFormat 测试，者使用线程封闭技术TreadLocal 达到多线程环境下线程安全
  *
  * @author caojx
- * @version $Id: SimpleDateFormatExample2.java,v 1.0 2019-07-25 16:23 caojx
+ * @version $Id: SimpleDateFormatExample3.java,v 1.0 2019-07-25 16:23 caojx
  * @date 2019-07-25 16:23
  */
 @Slf4j
 @ThreadSafe
-public class SimpleDateFormatExample2 {
+public class SimpleDateFormatExample3 {
 
     // 请求总数
     public static int clientTotal = 5000;
 
     // 同时并发执行的线程数
     public static int threadTotal = 200;
+
+    private static final ThreadLocal<SimpleDateFormat> simpleDateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyyMMdd");
+        }
+    };
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
@@ -48,13 +54,9 @@ public class SimpleDateFormatExample2 {
         executorService.shutdown();
     }
 
-    /**
-     * 将SimpleDateFormat声明为局部变量达到堆栈封闭，达到多线程环境下线程安全
-     */
     private static void update() {
         try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-            simpleDateFormat.parse("20180208");
+            simpleDateFormatThreadLocal.get().parse("20180208");
         } catch (ParseException e) {
             log.error("parse exception", e);
         }
