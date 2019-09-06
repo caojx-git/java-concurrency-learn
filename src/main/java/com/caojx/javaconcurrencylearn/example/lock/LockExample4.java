9b8cf99e83c25b16ff42b1dd41be64e6262a774f
jianx.cao001 <jianx.cao001@bkjk.com>
2019-09-06 18:54:10 +0800

    增加注释

--- src/main/java/com/caojx/javaconcurrencylearn/example/lock/LockExample4.java
+++ src/main/java/com/caojx/javaconcurrencylearn/example/lock/LockExample4.java
@@ -12,7 +12,7 @@ public class LockExample4 {
         private final StampedLock sl = new StampedLock();
 
         void move(double deltaX, double deltaY) { // an exclusively locked method
-            long stamp = sl.writeLock();
+            long stamp = sl.writeLock(); //涉及对共享资源的修改，使用写锁-独占操作
             try {
                 x += deltaX;
                 y += deltaY;
@@ -21,11 +21,18 @@ public class LockExample4 {
             }
         }
 
-        //下面看看乐观读锁案例
+        /**
+         * 下面看看乐观读锁案例
+         * 使用乐观读锁访问共享资源
+         * 注意：乐观读锁在保证数据一致性上需要拷贝一份要操作的变量到方法栈，并且在操作数据时候可能其他写线程已经修改了数据，
+         * 而我们操作的是方法栈里面的数据，也就是一个快照，所以最多返回的不是最新的数据，但是一致性还是得到保障的。
+         *
+         * @return
+         */
         double distanceFromOrigin() { // A read-only method
             long stamp = sl.tryOptimisticRead(); //获得一个乐观读锁
-            double currentX = x, currentY = y;  //将两个字段读入本地局部变量
-            if (!sl.validate(stamp)) { //检查发出乐观读锁后同时是否有其他写锁发生？
+            double currentX = x, currentY = y;  //拷贝共享资源到本地方法栈中
+            if (!sl.validate(stamp)) { //如果有写锁被占用，可能造成数据不一致，所以要切换到普通读锁模式
                 stamp = sl.readLock();  //如果没有，我们再次获得一个读悲观锁
                 try {
                     currentX = x; // 将两个字段读入本地局部变量
